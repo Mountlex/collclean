2826c43e8ea2fcd18482fb764d3e5c2a0c6deff4
Alexander Lindermayr <alexander.lindermayr97@gmail.com>
2022-03-18 19:19:17 +0100

    Minor improvements

--- README.md
+++ README.md
@@ -1,26 +1,30 @@
 # collclean
 
+[![crates.io](https://img.shields.io/crates/v/collclean.svg)](https://crates.io/crates/collclean)
+![actively developed](https://img.shields.io/badge/maintenance-actively--developed-brightgreen.svg)
+[![dependency status](https://deps.rs/crate/collclean/0.2.0/status.svg)](https://deps.rs/crate/collclean/0.2.0)
+![License: MIT/Apache-2.0](https://img.shields.io/crates/l/collclean.svg)
+
 Installation:
 
-```
+```bash
 cargo install collclean
 ```
 
 Example call
 
-```
-collclean file.tex \\mycomm1 \\mycomm2 ...
+```bash
+collclean file.tex mycomm1 mycomm2 ...
 ```
 
-File before (must compile!)
+`file.tex` (if it does not compile, collclean will remove nothing)
 
 ```tex
 \mycomm1{I wrote that!}
 \mycomm2{I wrote that! \mycomm2{lalalala} y\{y{y}y{} }
 ```
 
-
-File afterwards 
+`file.tex` afterwards
 
 ```tex
 I wrote that!
@@ -28,3 +32,9 @@ I wrote that! lalalala y\{y{y}y{}
 ```
 
 The command definitions (e.g. via `\newcommand`) will not get removed.
+
+Output to new file
+
+```bash
+collclean file.tex mycomm1 -o new_file.tex
+```
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use anyhow::{bail, Result};
 use clap::{arg, Command};
 
 fn main() -> Result<()> {
@@ -7,7 +7,14 @@ fn main() -> Result<()> {
         .author("Alexander Lindermayr <alexander.lindermayr97@gmail.com>")
         .about("Clean LaTeX files after a collaboration.")
         .arg(arg!(<FILE>))
-        .arg(arg!([COMMANDS]).required(true).multiple_values(true).takes_value(true).allow_invalid_utf8(true))
+        .arg(
+            arg!([COMMANDS])
+                .required(true)
+                .multiple_values(true)
+                .takes_value(true)
+                .allow_invalid_utf8(true),
+        )
+        .arg(arg!(-o - -output[output]).allow_invalid_utf8(true))
         .get_matches();
 
     let path = matches.value_of_os("FILE").map(std::path::PathBuf::from);
@@ -15,8 +22,13 @@ fn main() -> Result<()> {
     if let Some(path) = path {
         if path.exists() {
             let mut text = std::fs::read_to_string(&path)?;
-            clean(&mut text, commands);
-            std::fs::write(path, text)?;
+            let num = clean(&mut text, commands)?;
+            println!("Removed {} commands!", num / 2);
+            if let Some(output) = matches.value_of_os("output").map(std::path::PathBuf::from) {
+                std::fs::write(output, text)?;
+            } else {
+                std::fs::write(path, text)?;
+            }
         }
     }
     Ok(())
@@ -96,12 +108,12 @@ impl Deletion {
     }
 }
 
-fn clean(text: &mut String, commands: Vec<&str>) {
+fn clean(text: &mut String, commands: Vec<&str>) -> Result<usize> {
     let mut patterns: Vec<Pattern> = commands
         .into_iter()
         .map(|comm| {
             Pattern::new(
-                &format!("{}{{", comm),
+                &format!("\\{}{{", comm),
                 BracketCount::CountUp,
                 Process::Delete,
             )
@@ -123,44 +135,69 @@ fn clean(text: &mut String, commands: Vec<&str>) {
 
     let mut deleted_depths: Vec<usize> = vec![];
 
+    let mut commented = false;
+
+    let mut last_c: Option<char> = None;
+
     for (i, c) in text.char_indices() {
-        'patterns: for p in patterns.iter_mut() {
-            if let Some(s) = p.next(i, c) {
-                match (&p.processing, &p.count) {
-                    (Process::Keep, BracketCount::CountUp) => depth += 1,
-                    (Process::Keep, BracketCount::CountDown) => depth -= 1,
-                    (Process::Delete, BracketCount::CountUp) => {
-                        deleted_depths.push(depth);
-                        depth += 1;
-                        let deletion = Deletion::range(s, s + p.len() - 1);
-                        deletions.push(deletion);
-                    }
-                    (Process::DeleteOnDepth, BracketCount::CountDown) => {
-                        depth -= 1;
-                        if let Some(last) = deleted_depths.last() {
-                            if *last == depth {
-                                deleted_depths.remove(deleted_depths.len() - 1);
-                                let deletion = Deletion::range(s, s + p.len() - 1);
-                                deletions.push(deletion);
+        if c == '\n' {
+            commented = false;
+        }
+
+        if c == '%' && last_c != Some('\\') {
+            commented = true;
+        }
+        last_c = Some(c);
+
+        if !commented {
+            'patterns: for p in patterns.iter_mut() {
+                if let Some(s) = p.next(i, c) {
+                    match (&p.processing, &p.count) {
+                        (Process::Keep, BracketCount::CountUp) => depth += 1,
+                        (Process::Keep, BracketCount::CountDown) => {
+                            if depth == 0 {
+                                bail!("It seems that there is a opening bracket without closing counterpart! Stopping! (no changes made) {}", &text[(i.max(10) - 10)..(i+10).min(text.len() - 1)])
+                            }
+                            depth -= 1
+                        }
+                        (Process::Delete, BracketCount::CountUp) => {
+                            deleted_depths.push(depth);
+                            depth += 1;
+                            let deletion = Deletion::range(s, s + p.len() - 1);
+                            deletions.push(deletion);
+                        }
+                        (Process::DeleteOnDepth, BracketCount::CountDown) => {
+                            if depth == 0 {
+                                bail!("It seems that there is a opening bracket without closing counterpart! Stopping! (no changes made) {}", &text[(i.max(10) - 10)..(i+10).min(text.len() - 1)])
+                            }
+                            depth -= 1;
+                            if let Some(last) = deleted_depths.last() {
+                                if *last == depth {
+                                    deleted_depths.remove(deleted_depths.len() - 1);
+                                    let deletion = Deletion::range(s, s + p.len() - 1);
+                                    deletions.push(deletion);
+                                }
                             }
                         }
+                        (_, _) => {}
                     }
-                    (_, _) => {}
-                }
 
-                break 'patterns;
+                    break 'patterns;
+                }
             }
         }
     }
 
     let mut deleted: usize = 0;
+    let num = deletions.len();
     for del in deletions {
-        //println!("del from {} to {}", del.start, del.end);
         let start = del.start - deleted;
         let end = del.end - deleted;
         text.replace_range(start..=end, "");
         deleted += del.len();
     }
+
+    Ok(num)
 }
 
 #[cfg(test)]
@@ -168,33 +205,66 @@ mod test_clean {
     use super::*;
 
     #[test]
-    fn test_no_clean() {
+    fn test_no_clean() -> Result<()> {
         let mut text1 = String::from("\\{  \\}");
         let mut text2 = String::from("{  }");
-        clean(&mut text1, vec![]);
-        clean(&mut text2, vec![]);
+        clean(&mut text1, vec![])?;
+        clean(&mut text2, vec![])?;
         assert_eq!(text1, "\\{  \\}");
         assert_eq!(text2, "{  }");
+        Ok(())
     }
 
     #[test]
-    fn test_simple_clean() {
+    fn test_simple_clean() -> Result<()> {
         let mut text = String::from("\\anew{ab}");
-        clean(&mut text, vec!["\\anew"]);
+        clean(&mut text, vec!["anew"])?;
         assert_eq!(text, "ab");
+        Ok(())
     }
 
     #[test]
-    fn test_clean() {
-        let mut text = String::from("\\anew{ a{v}b \\{ }");
-        clean(&mut text, vec!["\\anew"]);
-        assert_eq!(text, " a{v}b \\{ ");
+    fn test_clean() -> Result<()> {
+        let mut text = String::from("\\anew{ a{v}b \\% \\{ }");
+        clean(&mut text, vec!["anew"])?;
+        assert_eq!(text, " a{v}b \\% \\{ ");
+        Ok(())
     }
 
     #[test]
-    fn test_clean_2() {
+    fn test_clean_2() -> Result<()> {
         let mut text = String::from("\\anew{\\nnew{ a{v}b} \\{ }");
-        clean(&mut text, vec!["\\anew", "\\nnew"]);
+        clean(&mut text, vec!["anew", "nnew"])?;
         assert_eq!(text, " a{v}b \\{ ");
+        Ok(())
+    }
+
+    #[test]
+    fn test_clean_fails() -> Result<()> {
+        let mut text = String::from("{ } }");
+        assert!(clean(&mut text, vec!["anew"]).is_err());
+        Ok(())
+    }
+
+    #[test]
+    fn test_clean_fails_2() -> Result<()> {
+        let mut text = String::from("\\anew{ } }");
+        assert!(clean(&mut text, vec!["anew"]).is_err());
+        Ok(())
+    }
+
+    #[test]
+    fn test_clean_no_fail() -> Result<()> {
+        let mut text = String::from("\\}\\}\\}");
+        assert!(clean(&mut text, vec!["anew"]).is_ok());
+        Ok(())
+    }
+
+    #[test]
+    fn test_clean_no_comment() -> Result<()> {
+        let mut text = String::from("% \\anew{ } \n");
+        clean(&mut text, vec!["anew"])?;
+        assert_eq!(text, "% \\anew{ } \n");
+        Ok(())
     }
 }
