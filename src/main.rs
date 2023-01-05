a0bdf4c7c4a62e54bb5cfabc67102cb85a6de33b
Alexander Lindermayr <linderal@uni-bremen.de>
2023-01-05 17:56:29 +0100

    Add report of deletions as output

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use std::fmt::Write;
+
 use anyhow::{bail, Result};
 use clap::{arg, Command};
+use yansi::{Paint, Style};
 
 fn main() -> Result<()> {
     let matches = Command::new("collclean")
@@ -22,7 +25,9 @@ fn main() -> Result<()> {
     if let Some(path) = path {
         if path.exists() {
             let mut text = std::fs::read_to_string(&path)?;
-            let num = clean(&mut text, commands)?;
+            let deletions = find_deletions(&text, commands)?;
+            print_deletions(&text, &deletions)?;
+            let num = clean_text(&mut text, deletions)?;
             println!("Removed {} commands!", num / 2);
             if let Some(output) = matches
                 .get_one::<String>("output")
@@ -78,6 +83,7 @@ impl Pattern {
     }
 }
 
+#[derive(Debug, Clone, PartialEq, PartialOrd, Ord, Eq)]
 struct Deletion {
     start: usize,
     end: usize,
@@ -98,7 +104,7 @@ enum Type {
     Other,
 }
 
-fn clean(text: &mut String, commands: Vec<&str>) -> Result<usize> {
+fn find_deletions(text: &str, commands: Vec<&str>) -> Result<Vec<Deletion>> {
     let mut patterns: Vec<(Pattern, Type)> = commands
         .into_iter()
         .map(|comm| (Pattern::new(&format!("\\{}{{", comm)), Type::Command))
@@ -158,6 +164,108 @@ fn clean(text: &mut String, commands: Vec<&str>) -> Result<usize> {
         bail!("It seems that there is a opening bracket without closing counterpart! Stopping! (no changes made)")
     }
 
+    deletions.sort();
+
+    Ok(deletions)
+}
+
+fn print_deletions(text: &str, deletions: &[Deletion]) -> Result<()> {
+    if deletions.is_empty() {
+        println!("No commands have been found!");
+        Ok(())
+    } else {
+        let mut line_start: usize = 0;
+        let mut deletions_iter = deletions.iter();
+        let mut current = deletions_iter.next();
+
+        for (l, line) in text.lines().enumerate() {
+            if current.is_none() {
+                break;
+            }
+
+            let line_len = line.len();
+            let line_end = line_start + line_len;
+            let mut dels = vec![];
+
+            while let Some(del) = current {
+                if line_start <= del.start && del.end <= line_end {
+                    dels.push(del);
+                    current = deletions_iter.next();
+                } else {
+                    break;
+                }
+            }
+
+            if !dels.is_empty() {
+                let mut string = String::new();
+                let line_str = format!("{}", Style::default().dimmed().paint(format!("L{}: ", l)));
+                string.write_str(&line_str)?;
+
+                let first_part = &text[line_start..dels.first().unwrap().start];
+                add_part(first_part, &mut string, Side::Left)?;
+                let first_del = &text[dels.first().unwrap().start..=dels.first().unwrap().end];
+                add_del(first_del, &mut string)?;
+
+                for w in dels.windows(2) {
+                    let gap = &text[w[0].end + 1..w[1].start];
+                    add_part(gap, &mut string, Side::Center)?;
+                    let del = &text[w[1].start..=w[1].end];
+                    add_del(del, &mut string)?;
+                }
+
+                let last_part = &text[dels.last().unwrap().end + 1..line_end];
+                add_part(last_part, &mut string, Side::Right)?;
+
+                string.retain(|c| c != '\n');
+
+                println!("{}", string);
+            }
+
+            line_start = line_end + 1;
+        }
+        Ok(())
+    }
+}
+
+fn add_del(part: &str, string: &mut String) -> Result<()> {
+    string.write_str(&format!("{}", Paint::red(part).bold()))?;
+    Ok(())
+}
+
+enum Side {
+    Left,
+    Center,
+    Right,
+}
+
+fn add_part(part: &str, string: &mut String, side: Side) -> Result<()> {
+    if part.len() > 10 {
+        match side {
+            Side::Left => {
+                let w = &part[part.len() - 10..];
+                string.write_str(&format!("... {}", w))?;
+            }
+            Side::Center => {
+                if part.len() <= 30 {
+                    string.write_str(part)?;
+                } else {
+                    let w1 = &part[..10];
+                    let w2 = &part[part.len() - 10..];
+                    string.write_str(&format!("{} ... {}", w1, w2))?;
+                }
+            }
+            Side::Right => {
+                let w = &part[..10];
+                string.write_str(&format!("{} ...", w))?;
+            }
+        }
+    } else {
+        string.write_str(part)?;
+    }
+    Ok(())
+}
+
+fn clean_text(text: &mut String, deletions: Vec<Deletion>) -> Result<usize> {
     let mut deleted: usize = 0;
     let num = deletions.len();
     for del in deletions {
@@ -173,6 +281,11 @@ fn clean(text: &mut String, commands: Vec<&str>) -> Result<usize> {
 #[cfg(test)]
 mod test_clean {
     use super::*;
+
+    fn clean(text: &mut String, commands: Vec<&str>) -> Result<usize> {
+        let deletions = find_deletions(text, commands)?;
+        clean_text(text, deletions)
+    }
 
     #[test]
     fn test_no_clean() -> Result<()> {
